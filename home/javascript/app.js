2cccc22c76007adc69bd452621ce8cc70f99a43c
kidnap 3 <110296316+notkidnap@users.noreply.github.com>
2023-06-26 18:00:38 -0400

    Add files via upload

---
+++ home/javascript/app.js
@@ -0,0 +1,18 @@
+// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+// Credits to expl0it, shellcode.team
+// GitHub: https://github.com/Lumm1t/obnoxious.club
+
+class _app {
+  id = 0;
+  videoElement = null;
+  audioElement = null;
+  musicVolume = 0.2;
+  musicFadeIn = 3000;
+  skippedIntro = false;
+  backgroundToggler = false;
+  shouldIgnoreVideo = false;
+  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+  brandDescription = ['/locate on top', 'we up', 'public party', '⛧'];
+}
+
+const app = new _app();
---
+++ home/javascript/portfolio.js
@@ -0,0 +1,193 @@
+/*
+
+Credits:
+    - Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+    - expl0it, shellcode.team
+    - Steam (emoticon: https://steamcommunity-a.akamaihd.net/economy/emoticon/mgh_17)
+    - Trollface image (http://www.rw-designer.com/icon-image/7835-256x256x32.png)
+    - Tumblr images/icons:
+        a) https://68.media.tumblr.com/730ba51e7f6b0203e023deeb0db8367b/tumblr_osgbbiLXWV1suieauo1_500.jpg
+        b) https://68.media.tumblr.com/8436227895295acecdea170b612fb766/tumblr_nxgg5bYqm81rclv0wo1_500.jpg
+        c) http://images.neon-sign.org/l-m/rose-flower-love-neon-sign.jpg
+        d) https://media.istockphoto.com/vectors/rose-neon-sign-vector-id1088857104?k=6&m=1088857104&s=612x612&w=0&h=ZJCYow5i8noF6nTGHEd6b2bbe9X6rVx4cLDoceexAec=
+        e) https://image.freepik.com/free-vector/polygonal-geometric-rose-neon-sign_1262-19626.jpg
+        f) https://68.media.tumblr.com/49394d5aa66d4c7ea4ffebda38c6be7e/tumblr_orv2rd9eVC1rgewhto1_400.gif
+        g) https://ih1.redbubble.net/image.268210271.5200/sticker,375x360-bg,ffffff.u2.png
+        h) http://picture-cdn.wheretoget.it/ot63b4-l-610x610-shirt-rose-tumblr-grunge-grunge+t+shirt-clothes-t+shirt-tumblr+shirt-dead+roses-black+bear-soft+grunge-soft-soft+ghetto-ghetto-chill-cute-white-red-red+roses-white+tee-pale-sad-dar.jpg
+
+Thanks for:
+    - Google
+    - StackOverflow
+    - jQuery
+    - jQuery Marquee
+    - animate.css
+    - typed.js
+
+GitHub: https://github.com/Lumm1t/obnoxious.club
+
+*/
+
+'use strict';
+
+const timeouts = [];
+
+const mobileAndTabletCheck = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+$(document).ready(() => {
+  const links = [
+    {
+      name: '@zai',
+      link: 'info',
+    },
+    {
+      name: '@swaz',
+      link: 's',
+    },
+    {
+      name: '@haven',
+      link: 'HaVen',
+    },
+    {
+      name: '@gram',
+      link: 'gram',
+    },
+  ];
+
+  for (let i in links) {
+    let link = links[i];
+
+    $('#marquee').append(`<a href="https://swazypooh.netlify.app/${link.link}" target="_BLANK">${link.name}</a>`);
+
+    link = $('#marquee').children('a').last();
+
+    if (i != links.length - 1) $('#marquee').append(' <img class="emoticon" src="home/others/1039354050665267201.png"> ');
+  }
+
+  if (mobileAndTabletCheck()) {
+    $('#background').replaceWith('<div id="background" style="background-image: url(attachments/270957.png);"></div>');
+
+    app.shouldIgnoreVideo = true;
+  }
+
+});
+
+if ($.cookie('videoTime')) {
+  app.videoElement.currentTime = $.cookie('videoTime');
+  app.audioElement.currentTime = $.cookie('videoTime');
+}
+
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+});
+
+document.body.onkeyup = (event) => {
+  if (event.keyCode == 32 && app.skippedIntro) {
+    if (app.backgroundToggler) {
+      app.videoElement.play();
+      app.audioElement.play();
+    } else {
+      app.videoElement.pause();
+      app.audioElement.pause();
+    }
+
+    return (app.backgroundToggler = !app.backgroundToggler);
+  }
+};
+
+$('.skip').click(() => {
+  skipIntro();
+});
+
+$.fn.extend({
+  animateCss: function (animationName) {
+    const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+    this.addClass(`animated ${animationName}`).one(animationEnd, () => {
+      $(this).removeClass(`animated ${animationName}`);
+    });
+
+    return this;
+  },
+});
+
+const writeLine = (text, speed, timeout, callback) => {
+  timeout = typeof timeout === 'number' ? timeout : [0, (callback = timeout)];
+
+  const lineNumber = app.id !== 2 ? ++app.id : (app.id += 2);
+
+  setTimeout(() => {
+    const typed = new Typed(`#line${lineNumber}`, {
+      strings: text,
+      typeSpeed: speed,
+      onComplete: callback,
+    });
+  }, timeout);
+};
+
+const skipIntro = () => {
+  if (app.skippedIntro) return;
+
+  app.skippedIntro = true;
+
+  timeouts.forEach((timeout) => {
+    clearTimeout(timeout);
+  });
+
+  $('.top-right').remove();
+
+  $('#main').fadeOut(100, () => {
+    $('#main').remove();
+
+    $('#marquee').marquee({
+      duration: 15000,
+      gap: 420,
+      delayBeforeStart: 1000,
+      direction: 'left',
+      duplicated: true,
+    });
+
+    setTimeout(() => {
+      $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
+    }, 200);
+
+    setTimeout(() => {
+      const typed = new Typed('#brand', {
+        strings: app.brandDescription,
+        typeSpeed: 60,
+
+        onComplete: () => {
+          clearCursor();
+        },
+      });
+    }, 1350);
+
+    setTimeout(() => {
+      if (!app.shouldIgnoreVideo) {
+        app.videoElement.play();
+        app.audioElement.play();
+      }
+
+      app.videoElement.addEventListener(
+        'timeupdate',
+        () => {
+          $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
+        },
+        false
+      );
+
+      $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
+
+      $('.marquee-container').animateCss('zoomIn');
+
+      $('.container').fadeIn();
+
+      $('.background').fadeIn(200, () => {
+        if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
+      });
+    }, 300);
+  });
+};
+
+const clearCursor = () => {
+  return $('span').siblings('.typed-cursor').css('opacity', '0');
+};
